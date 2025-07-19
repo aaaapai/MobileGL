6f455bc26a6b77510a28401ef7b5eb5e38f1845e
Swung0x48 <swung0x48@outlook.com>
2025-07-19 21:33:28 +0800

    [Test] (MG_Test/Program): add Sanity test

---
+++ MobileGL/MG_Test/Program/ProgramTest.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Swung 0x48 on 2025/7/17.
+//
+#include <gtest/gtest.h>
+
+#include "Includes.h"
+
+using namespace MobileGL;
+
+class ProgramTest : public ::testing::Test {
+protected:
+};
+
+TEST_F(ProgramTest, Sanity) {
+    ASSERT_TRUE(true);
+}
