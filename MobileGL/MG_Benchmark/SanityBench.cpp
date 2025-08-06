3606b8001698154eddf9582ac7de50f92a036071
Swung0x48 <swung0x48@outlook.com>
2025-08-06 13:25:24 +0800

    [Chore] (MG_Benchmark): add benchmark

---
+++ MobileGL/MG_Benchmark/SanityBench.cpp
@@ -0,0 +1,31 @@
+#include <benchmark/benchmark.h>
+#include <vector>
+
+static void Sanity_VectorResize(benchmark::State& state) {
+    for (auto _ : state) {
+        std::vector<int> v;
+        v.resize(state.range(0), 0);
+        for (int i = 0; i < state.range(0); i += 16) {
+            v[i] = i;
+        }
+    }
+    state.SetBytesProcessed(int64_t(state.iterations()) *
+                            int64_t(state.range(0)));
+}
+BENCHMARK(Sanity_VectorResize)->Arg(2<<5)->Arg(2<<10)->Arg(2<<20);
+
+
+static void Sanity_memcpy(benchmark::State& state) {
+    char* src = new char[state.range(0)];
+    char* dst = new char[state.range(0)];
+    memset(src, 'x', state.range(0));
+    for (auto _ : state)
+        memcpy(dst, src, state.range(0));
+    state.SetBytesProcessed(int64_t(state.iterations()) *
+                            int64_t(state.range(0)));
+    delete[] src;
+    delete[] dst;
+}
+BENCHMARK(Sanity_memcpy)->Range(8, 8<<10);
+
+BENCHMARK_MAIN();
