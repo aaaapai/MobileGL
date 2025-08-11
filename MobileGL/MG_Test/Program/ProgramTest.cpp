99a87265067018a57c5c89360d9b364ae1e016e2
Swung0x48 <swung0x48@outlook.com>
2025-08-11 12:52:44 +0800

    [Chore] (MG_Test): move library finding to parent CMakeList and link libraries to all the test targets

---
+++ MobileGL/MG_Test/Program/ProgramTest.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+#include "Includes.h"
+
+using namespace MobileGL;
+
+class ProgramTest : public ::testing::Test {
+protected:
+};
+
+TEST_F(ProgramTest, Sanity) {
+    ASSERT_TRUE(true);
+}
