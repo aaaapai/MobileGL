0a95d64f2b648956dfb7d648232df650ece9ea17
BZLZHH <admin@bzlzhh.top>
2025-08-12 15:28:12 +0800

    [Chore] (CMake): Support more platforms.

--- MobileGL/MG_Impl/GLImpl/Texture/GL_Texture.cpp
+++ MobileGL/MG_Impl/GLImpl/Texture/GL_Texture.cpp
@@ -3,78 +3,87 @@
 namespace MobileGL {
     namespace MG_Impl::GLImpl {
         /* @INSERTION_POINT:FUNCTION_IMPLEMENTATION@ */
-        void TexSubImage3D(GLenum target, GLint level, GLint xoffset, GLint yoffset, GLint zoffset, GLsizei width, GLsizei height, GLsizei depth, GLenum format, GLenum type, const void* pixels) {
+        void TexSubImage3D(GLenum target, GLint level, GLint xoffset, GLint yoffset, GLint zoffset, GLsizei width,
+                           GLsizei height, GLsizei depth, GLenum format, GLenum type, const void* pixels) {
             // TODO: implement
         }
-        
-        void TexSubImage2D(GLenum target, GLint level, GLint xoffset, GLint yoffset, GLsizei width, GLsizei height, GLenum format, GLenum type, const void* pixels) {
+
+        void TexSubImage2D(GLenum target, GLint level, GLint xoffset, GLint yoffset, GLsizei width, GLsizei height,
+                           GLenum format, GLenum type, const void* pixels) {
             // TODO: implement
         }
-        
-        void TexSubImage1D(GLenum target, GLint level, GLint xoffset, GLsizei width, GLenum format, GLenum type, const GLvoid* pixels) {
+
+        void TexSubImage1D(GLenum target, GLint level, GLint xoffset, GLsizei width, GLenum format, GLenum type,
+                           const GLvoid* pixels) {
             // TODO: implement
         }
-        
+
         void TexParameterf(GLenum target, GLenum pname, GLfloat param) {
             // TODO: implement
         }
-        
+
         void TexParameteri(GLenum target, GLenum pname, GLint param) {
             // TODO: implement
         }
-        
-        void TexImage3DMultisample(GLenum target, GLsizei samples, GLenum internalformat, GLsizei width, GLsizei height, GLsizei depth, GLboolean fixedsamplelocations) {
+
+        void TexImage3DMultisample(GLenum target, GLsizei samples, GLenum internalformat, GLsizei width, GLsizei height,
+                                   GLsizei depth, GLboolean fixedsamplelocations) {
             // TODO: implement
         }
-        
-        void TexImage2DMultisample(GLenum target, GLsizei samples, GLenum internalformat, GLsizei width, GLsizei height, GLboolean fixedsamplelocations) {
+
+        void TexImage2DMultisample(GLenum target, GLsizei samples, GLenum internalformat, GLsizei width, GLsizei height,
+                                   GLboolean fixedsamplelocations) {
             // TODO: implement
         }
-        
-        void TexImage3D(GLenum target, GLint level, GLint internalformat, GLsizei width, GLsizei height, GLsizei depth, GLint border, GLenum format, GLenum type, const void* pixels) {
+
+        void TexImage3D(GLenum target, GLint level, GLint internalformat, GLsizei width, GLsizei height, GLsizei depth,
+                        GLint border, GLenum format, GLenum type, const void* pixels) {
             // TODO: implement
         }
-        
-        void TexImage2D(GLenum target, GLint level, GLint internalformat, GLsizei width, GLsizei height, GLint border, GLenum format, GLenum type, const void* pixels) {
+
+        void TexImage2D(GLenum target, GLint level, GLint internalformat, GLsizei width, GLsizei height, GLint border,
+                        GLenum format, GLenum type, const void* pixels) {
             // TODO: implement
         }
-        
-        void TexImage1D(GLenum target, GLint level, GLint internalFormat, GLsizei width, GLint border, GLenum format, GLenum type, const GLvoid* pixels) {
+
+        void TexImage1D(GLenum target, GLint level, GLint internalFormat, GLsizei width, GLint border, GLenum format,
+                        GLenum type, const GLvoid* pixels) {
             // TODO: implement
         }
-        
+
         void TexBuffer(GLenum target, GLenum internalformat, GLuint buffer) {
             // TODO: implement
         }
-        
+
         GLboolean IsTexture(GLuint texture) {
             // TODO: implement
+            return GL_FALSE;
         }
-        
+
         void GetTexParameterIuiv(GLenum target, GLenum pname, GLuint* params) {
             // TODO: implement
         }
-        
+
         void GetTexParameterIiv(GLenum target, GLenum pname, GLint* params) {
             // TODO: implement
         }
-        
+
         void GetTexParameteriv(GLenum target, GLenum pname, GLint* params) {
             // TODO: implement
         }
-        
+
         void GetTexParameterfv(GLenum target, GLenum pname, GLfloat* params) {
             // TODO: implement
         }
-        
+
         void GetTexLevelParameteriv(GLenum target, GLint level, GLenum pname, GLint* params) {
             // TODO: implement
         }
-        
+
         void GetTexLevelParameterfv(GLenum target, GLint level, GLenum pname, GLfloat* params) {
             // TODO: implement
         }
-        
+
         void GetTexImage(GLenum target, GLint level, GLenum format, GLenum type, GLvoid* pixels) {
             // TODO: implement
         }
