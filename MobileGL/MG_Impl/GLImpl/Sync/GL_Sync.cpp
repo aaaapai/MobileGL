e190f3887d8ea35ab8eafa47294d1b60025569a8
Swung0x48 <swung0x48@outlook.com>
2025-10-30 22:59:39 +0800

    [Feat] (GLImpl/Sync): manifold for Sync

---
+++ MobileGL/MG_Impl/GLImpl/Sync/GL_Sync.cpp
@@ -0,0 +1,49 @@
+#include "GL_Sync.h"
+
+#include "MG_State/GLState/Core.h"
+
+namespace MobileGL {
+    namespace MG_Impl::GLImpl {
+        GLsync FenceSync_Backend(GLenum condition, GLbitfield flags) {
+
+        }
+
+        GLenum ClientWaitSync_Backend(GLsync sync,
+                                    GLbitfield flags,
+                                    GLuint64 timeout) {
+
+        }
+
+        void DeleteSync_Backend(GLsync sync) {
+
+        }
+
+        GLsync FenceSync_State(GLenum condition, GLbitfield flags) {
+
+        }
+
+        GLenum ClientWaitSync_State(GLsync sync,
+                              GLbitfield flags,
+                              GLuint64 timeout) {
+
+        }
+
+        void DeleteSync_State(GLsync sync) {
+
+        }
+
+        GLsync FenceSync(GLenum condition, GLbitfield flags) {
+
+        }
+
+        GLenum ClientWaitSync(GLsync sync,
+                              GLbitfield flags,
+                              GLuint64 timeout) {
+
+        }
+
+        void DeleteSync(GLsync sync) {
+
+        }
+    } // namespace MG_Impl::GLImpl
+} // namespace MobileGL
