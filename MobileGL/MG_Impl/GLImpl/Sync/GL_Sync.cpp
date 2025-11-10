fd3b5c38814b4d1462a6d374cc47f81b3ee4aecc
Swung0x48 <swung0x48@outlook.com>
2025-11-10 15:49:26 +0800

    [Fix] (MG_Impl/GL_Sync): fix compilation error on msvc

--- MobileGL/MG_Impl/GLImpl/Sync/GL_Sync.cpp
+++ MobileGL/MG_Impl/GLImpl/Sync/GL_Sync.cpp
@@ -4,21 +4,33 @@
 
 namespace MobileGL {
     namespace MG_Impl::GLImpl {
-        GLsync FenceSync_Backend(GLenum condition, GLbitfield flags) {}
+        GLsync FenceSync_Backend(GLenum condition, GLbitfield flags) {
+            return 0;
+        }
 
-        GLenum ClientWaitSync_Backend(GLsync sync, GLbitfield flags, GLuint64 timeout) {}
+        GLenum ClientWaitSync_Backend(GLsync sync, GLbitfield flags, GLuint64 timeout) {
+            return 0;
+        }
 
         void DeleteSync_Backend(GLsync sync) {}
 
-        GLsync FenceSync_State(GLenum condition, GLbitfield flags) {}
+        GLsync FenceSync_State(GLenum condition, GLbitfield flags) {
+            return 0;
+        }
 
-        GLenum ClientWaitSync_State(GLsync sync, GLbitfield flags, GLuint64 timeout) {}
+        GLenum ClientWaitSync_State(GLsync sync, GLbitfield flags, GLuint64 timeout) {
+            return 0;
+        }
 
         void DeleteSync_State(GLsync sync) {}
 
-        GLsync FenceSync(GLenum condition, GLbitfield flags) {}
+        GLsync FenceSync(GLenum condition, GLbitfield flags) {
+            return 0;
+        }
 
-        GLenum ClientWaitSync(GLsync sync, GLbitfield flags, GLuint64 timeout) {}
+        GLenum ClientWaitSync(GLsync sync, GLbitfield flags, GLuint64 timeout) {
+            return 0;
+        }
 
         void DeleteSync(GLsync sync) {}
     } // namespace MG_Impl::GLImpl
