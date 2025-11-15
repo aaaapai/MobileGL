a0cc3e5f3e1aae79199d5f3cc2fd212dfd86960d
BZLZHH <admin@bzlzhh.top>
2025-11-15 21:40:25 +0800

    [Misc] (MG_Impl/Getter): Hardcode the value returned by GL_UNIFORM_BUFFER_OFFSET_ALIGNMENT as 256.

--- MobileGL/MG_Impl/GLImpl/Getter/GL_Getter.cpp
+++ MobileGL/MG_Impl/GLImpl/Getter/GL_Getter.cpp
@@ -779,7 +779,7 @@ namespace MobileGL {
                 *params = 0; // TODO
                 break;
             case GL_UNIFORM_BUFFER_OFFSET_ALIGNMENT:
-                *params = 1; // TODO
+                *params = 256; // TODO: get real value from backend
                 break;
             case GL_UNIFORM_BUFFER_SIZE:
                 *params = 0; // TODO
