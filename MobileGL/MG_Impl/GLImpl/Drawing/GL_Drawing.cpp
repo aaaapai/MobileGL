407d57e9b957103a861f38a9dba637dd1d09b5e7
BZLZHH <admin@bzlzhh.top>
2025-12-07 10:50:26 +0800

    [Feat] (MG_Impl/Drawing): Implement more draw funcs.

--- MobileGL/MG_Impl/GLImpl/Drawing/GL_Drawing.cpp
+++ MobileGL/MG_Impl/GLImpl/Drawing/GL_Drawing.cpp
@@ -48,7 +48,146 @@ namespace MobileGL {
 #endif
         }
 
+        void MultiDrawElementsIndirect_Backend(GLenum mode, GLenum type, const void* indirect, GLsizei drawcount,
+                                               GLsizei stride) {
+#if MOBILEGL_BACKEND == MOBILEGL_BACKEND_TYPE_DIRECT_GLES
+            MG_Backend::DirectGLES::MultiDrawElementsIndirect(mode, type, indirect, drawcount, stride);
+#endif
+        }
+
+        void MultiDrawArraysIndirect_Backend(GLenum mode, const void* indirect, GLsizei drawcount, GLsizei stride) {
+#if MOBILEGL_BACKEND == MOBILEGL_BACKEND_TYPE_DIRECT_GLES
+            MG_Backend::DirectGLES::MultiDrawArraysIndirect(mode, indirect, drawcount, stride);
+#endif
+        }
+
+        void DrawRangeElementsBaseVertex_Backend(GLenum mode, GLuint start, GLuint end, GLsizei count, GLenum type,
+                                                 const void* indices, GLint basevertex) {
+#if MOBILEGL_BACKEND == MOBILEGL_BACKEND_TYPE_DIRECT_GLES
+            MG_Backend::DirectGLES::DrawRangeElementsBaseVertex(mode, start, end, count, type, indices, basevertex);
+#endif
+        }
+
+        void DrawRangeElements_Backend(GLenum mode, GLuint start, GLuint end, GLsizei count, GLenum type,
+                                       const void* indices) {
+#if MOBILEGL_BACKEND == MOBILEGL_BACKEND_TYPE_DIRECT_GLES
+            MG_Backend::DirectGLES::DrawRangeElements(mode, start, end, count, type, indices);
+#endif
+        }
+
+        void DrawElementsInstancedBaseVertexBaseInstance_Backend(GLenum mode, GLsizei count, GLenum type,
+                                                                 const void* indices, GLsizei instancecount,
+                                                                 GLint basevertex, GLuint baseinstance) {
+#if MOBILEGL_BACKEND == MOBILEGL_BACKEND_TYPE_DIRECT_GLES
+            MG_Backend::DirectGLES::DrawElementsInstancedBaseVertexBaseInstance(
+                mode, count, type, indices, instancecount, basevertex, baseinstance);
+#endif
+        }
+
+        void DrawElementsInstancedBaseVertex_Backend(GLenum mode, GLsizei count, GLenum type, const void* indices,
+                                                     GLsizei instancecount, GLint basevertex) {
+#if MOBILEGL_BACKEND == MOBILEGL_BACKEND_TYPE_DIRECT_GLES
+            MG_Backend::DirectGLES::DrawElementsInstancedBaseVertex(mode, count, type, indices, instancecount,
+                                                                    basevertex);
+#endif
+        }
+
+        void DrawElementsInstancedBaseInstance_Backend(GLenum mode, GLsizei count, GLenum type, const void* indices,
+                                                       GLsizei instancecount, GLuint baseinstance) {
+#if MOBILEGL_BACKEND == MOBILEGL_BACKEND_TYPE_DIRECT_GLES
+            MG_Backend::DirectGLES::DrawElementsInstancedBaseInstance(mode, count, type, indices, instancecount,
+                                                                      baseinstance);
+#endif
+        }
+
+        void DrawElementsInstanced_Backend(GLenum mode, GLsizei count, GLenum type, const void* indices,
+                                           GLsizei instancecount) {
+#if MOBILEGL_BACKEND == MOBILEGL_BACKEND_TYPE_DIRECT_GLES
+            MG_Backend::DirectGLES::DrawElementsInstanced(mode, count, type, indices, instancecount);
+#endif
+        }
+
+        void DrawElementsIndirect_Backend(GLenum mode, GLenum type, const void* indirect) {
+#if MOBILEGL_BACKEND == MOBILEGL_BACKEND_TYPE_DIRECT_GLES
+            MG_Backend::DirectGLES::DrawElementsIndirect(mode, type, indirect);
+#endif
+        }
+        void DrawArraysInstancedBaseInstance_Backend(GLenum mode, GLint first, GLsizei count, GLsizei instancecount,
+                                                     GLuint baseinstance) {
+#if MOBILEGL_BACKEND == MOBILEGL_BACKEND_TYPE_DIRECT_GLES
+            MG_Backend::DirectGLES::DrawArraysInstancedBaseInstance(mode, first, count, instancecount, baseinstance);
+#endif
+        }
+
+        void DrawArraysInstanced_Backend(GLenum mode, GLint first, GLsizei count, GLsizei instancecount) {
+#if MOBILEGL_BACKEND == MOBILEGL_BACKEND_TYPE_DIRECT_GLES
+            MG_Backend::DirectGLES::DrawArraysInstanced(mode, first, count, instancecount);
+#endif
+        }
+
+        void DrawArraysIndirect_Backend(GLenum mode, const void* indirect) {
+#if MOBILEGL_BACKEND == MOBILEGL_BACKEND_TYPE_DIRECT_GLES
+            MG_Backend::DirectGLES::DrawArraysIndirect(mode, indirect);
+#endif
+        }
+
         /* @INSERTION_POINT:FUNCTION_IMPLEMENTATION@ */
+        void MultiDrawElementsIndirect(GLenum mode, GLenum type, const void* indirect, GLsizei drawcount,
+                                       GLsizei stride) {
+            MultiDrawElementsIndirect_Backend(mode, type, indirect, drawcount, stride);
+        }
+
+        void MultiDrawArraysIndirect(GLenum mode, const void* indirect, GLsizei drawcount, GLsizei stride) {
+            MultiDrawArraysIndirect_Backend(mode, indirect, drawcount, stride);
+        }
+
+        void DrawRangeElementsBaseVertex(GLenum mode, GLuint start, GLuint end, GLsizei count, GLenum type,
+                                         const void* indices, GLint basevertex) {
+            DrawRangeElementsBaseVertex_Backend(mode, start, end, count, type, indices, basevertex);
+        }
+
+        void DrawRangeElements(GLenum mode, GLuint start, GLuint end, GLsizei count, GLenum type, const void* indices) {
+            DrawRangeElements_Backend(mode, start, end, count, type, indices);
+        }
+
+        void DrawElementsInstancedBaseVertexBaseInstance(GLenum mode, GLsizei count, GLenum type, const void* indices,
+                                                         GLsizei instancecount, GLint basevertex, GLuint baseinstance) {
+            DrawElementsInstancedBaseVertexBaseInstance_Backend(mode, count, type, indices, instancecount, basevertex,
+                                                                baseinstance);
+        }
+
+        void DrawElementsInstancedBaseVertex(GLenum mode, GLsizei count, GLenum type, const void* indices,
+                                             GLsizei instancecount, GLint basevertex) {
+            DrawElementsInstancedBaseVertex_Backend(mode, count, type, indices, instancecount, basevertex);
+        }
+
+        void DrawElementsInstancedBaseInstance(GLenum mode, GLsizei count, GLenum type, const void* indices,
+                                               GLsizei instancecount, GLuint baseinstance) {
+            DrawElementsInstancedBaseInstance_Backend(mode, count, type, indices, instancecount, baseinstance);
+        }
+
+        void DrawElementsInstanced(GLenum mode, GLsizei count, GLenum type, const void* indices,
+                                   GLsizei instancecount) {
+            DrawElementsInstanced_Backend(mode, count, type, indices, instancecount);
+        }
+
+        void DrawElementsIndirect(GLenum mode, GLenum type, const void* indirect) {
+            DrawElementsIndirect_Backend(mode, type, indirect);
+        }
+
+        void DrawArraysInstancedBaseInstance(GLenum mode, GLint first, GLsizei count, GLsizei instancecount,
+                                             GLuint baseinstance) {
+            DrawArraysInstancedBaseInstance_Backend(mode, first, count, instancecount, baseinstance);
+        }
+
+        void DrawArraysInstanced(GLenum mode, GLint first, GLsizei count, GLsizei instancecount) {
+            DrawArraysInstanced_Backend(mode, first, count, instancecount);
+        }
+
+        void DrawArraysIndirect(GLenum mode, const void* indirect) {
+            DrawArraysIndirect_Backend(mode, indirect);
+        }
+
         void DrawElementsBaseVertex(GLenum mode, GLsizei count, GLenum type, const void* indices, GLint basevertex) {
             DrawElementsBaseVertex_Backend(mode, count, type, indices, basevertex);
         }
