ee610cdf59c92ce1a758f7ea7d4cbd087a7d69fd
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-12-13 23:39:24 +0800

    Update DirectGLES.cpp

--- MobileGL/MG_Backend/DirectGLES/DirectGLES.cpp
+++ MobileGL/MG_Backend/DirectGLES/DirectGLES.cpp
@@ -11,6 +11,7 @@
 #include <MG_Util/Converters/MGToGL/TextureEnumConverter.h>
 #include <MG_Util/Converters/MGToStr/TextureEnumConverter.h>
 #include <MG_Util/Converters/MGToGL/RenderStateEnumConverter.h>
+#include "multidraw.h"
 
 namespace MobileGL::MG_Backend::DirectGLES {
     enum class DrawSyncBit : Uint32 {
@@ -620,9 +621,11 @@ namespace MobileGL::MG_Backend::DirectGLES {
         DrawSyncBit syncBit = DrawSyncBit::IndexBuffer;
         PrepareForDraw(syncBit);
 
-        for (GLsizei i = 0; i < drawcount; ++i) {
+        /*for (GLsizei i = 0; i < drawcount; ++i) {
             MG_External::GLES::glDrawElementsBaseVertex(mode, count[i], type, indices[i], basevertex[i]);
-        }
+        }*/
+
+        g_multiDrawElementsBaseVertex_shader.multiDrawElementsBaseVertex(mode, count, type, indices, drawcount, basevertex);
         
     }
 
