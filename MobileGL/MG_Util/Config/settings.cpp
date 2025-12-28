f48c023bde408cd0ce10caca6a8a1e8a30ac0c50
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-12-28 15:35:52 +0800

    Update settings.cpp

--- MobileGL/MG_Util/Config/settings.cpp
+++ MobileGL/MG_Util/Config/settings.cpp
@@ -4,10 +4,6 @@
 
 #include "settings.h"
 #include "config.h"
-#include "../gl/log.h"
-#include "../gl/envvars.h"
-#include "gpu_utils.h"
-#include "../gl/getter.h"
 
 #define DEBUG 0
 
@@ -95,38 +91,17 @@ void init_settings() {
 
     LOG_V("MG_DIR_PATH = %s", mg_directory_path ? mg_directory_path : "(default)")
 
-    if (isInPluginApp == 0 && fclVersion == 0 && zlVersion == 0 && pgwVersion == 0 && !is_custom_mg_dir) {
-        LOG_V("Unsupported launcher detected, force using default config.")
-        angleConfig = AngleConfig::DisableIfPossible;
-        noErrorConfig = NoErrorConfig::Auto;
-        enableExtGL43 = false;
-        enableExtComputeShader = false;
-        enableExtTimerQuery = true;
-        enableExtDirectStateAccess = true;
-        maxGlslCacheSize = 0;
-        angleDepthClearFixMode = AngleDepthClearFixMode::Disabled;
-        fsr1Setting = FSR1_Quality_Preset::Disabled;
-        hideMGEnvLevel = HideMGEnvLevel::Disabled;
-    }
-
     AngleMode finalAngleMode = AngleMode::Disabled;
     std::string gpuString = getGPUInfo();
     const char* gpu_cstr = gpuString.c_str();
     LOG_D("GPU: %s", gpu_cstr ? gpu_cstr : "(unknown)")
 
-    int hasVk12 = hasVulkan12();
-    int isQcom = isAdreno(gpu_cstr);
-    int is730 = isAdreno730(gpu_cstr);
-    int is740 = isAdreno740(gpu_cstr);
-    int is830 = isAdreno830(gpu_cstr);
-    bool isANGLESupported = checkIfANGLESupported(gpu_cstr);
-
-    LOG_D("Has Vulkan 1.2? = %s", hasVk12 ? "true" : "false")
-    LOG_D("Is Adreno? = %s", isQcom ? "true" : "false")
-    LOG_D("Is Adreno 730? = %s", is730 ? "true" : "false")
-    LOG_D("Is Adreno 740? = %s", is740 ? "true" : "false")
-    LOG_D("Is Adreno 830? = %s", is830 ? "true" : "false")
-    LOG_D("Is ANGLE supported? = %s", isANGLESupported ? "true" : "false")
+    int hasVk12 = 0;
+    int isQcom = 0;
+    int is730 = 0;
+    int is740 = 0;
+    int is830 = 0;
+    bool isANGLESupported = true;
 
     switch (angleConfig) {
     case AngleConfig::ForceDisable:
@@ -158,6 +133,7 @@ void init_settings() {
     if (global_settings.angle == AngleMode::Enabled) {
         // setenv("LIBGL_GLES", "libGLESv2_angle.so", 1);
         // setenv("LIBGL_EGL", "libEGL_angle.so", 1);
+        setenv("LIBGL_ANGLE", "1", 1);
     }
 
     switch (noErrorConfig) {
@@ -193,27 +169,20 @@ void init_settings() {
     LOG_V("[MobileGlues] Setting: enableAngle                 = %s",
           global_settings.angle == AngleMode::Enabled ? "true" : "false")
     LOG_V("[MobileGlues] Setting: ignoreError                 = %i", static_cast<int>(global_settings.ignore_error))
-    LOG_V("[MobileGlues] Setting: enableExtComputeShader      = %s",
-          global_settings.ext_compute_shader ? "true" : "false")
-    LOG_V("[MobileGlues] Setting: enableExtGL43               = %s", global_settings.ext_gl43 ? "true" : "false")
-    LOG_V("[MobileGlues] Setting: enableExtTimerQuery         = %s", global_settings.ext_timer_query ? "true" : "false")
-    LOG_V("[MobileGlues] Setting: enableExtDirectStateAccess  = %s",
-          global_settings.ext_direct_state_access ? "true" : "false")
-    LOG_V("[MobileGlues] Setting: maxGlslCacheSize            = %i",
-          static_cast<int>(global_settings.max_glsl_cache_size / 1024 / 1024))
-    LOG_V("[MobileGlues] Setting: angleDepthClearFixMode      = %i",
-          static_cast<int>(global_settings.angle_depth_clear_fix_mode))
-    LOG_V("[MobileGlues] Setting: bufferCoherentAsFlush       = %i",
-          static_cast<int>(global_settings.buffer_coherent_as_flush))
+    LOG_V("[MobileGL] Setting: enableExtComputeShader is unsupported.")
+    LOG_V("[MobileGL] Setting: enableExtGL43 is unsupported.")
+    LOG_V("[MobileGL] Setting: enableExtTimerQuery is unsupported.")
+    LOG_V("[MobileGL] Setting: enableExtDirectStateAccess is unsupported.")
+    LOG_V("[MobileGL] Setting: maxGlslCacheSize is unsupported.")
+    LOG_V("[MobileGL] Setting: angleDepthClearFixMode is unsupported.")
+    LOG_V("[MobileGL] Setting: bufferCoherentAsFlush is unsupported.")
     if (global_settings.custom_gl_version.isEmpty()) {
-        LOG_V("[MobileGlues] Setting: customGLVersion             = (default)");
+        LOG_V("[MobileGL] Setting: customGLVersion is unsupported.");
     } else {
-        LOG_V("[MobileGlues] Setting: customGLVersion             = %s",
-              global_settings.custom_gl_version.toString().c_str());
+        LOG_V("[MobileGL] Setting: customGLVersion is unsupported.");
     }
-    LOG_V("[MobileGlues] Setting: fsr1Setting                 = %i", static_cast<int>(global_settings.fsr1_setting))
-    LOG_V("[MobileGlues] Setting: hideMGEnvLevel              = %i",
-          static_cast<int>(global_settings.hide_mg_env_level))
+    LOG_V("[MobileGL] Setting: fsr1Setting is unsupported.")
+    LOG_V("[MobileGL] Setting: hideMGEnvLevel is unsupported.")
 
     GLVersion =
         global_settings.custom_gl_version.isEmpty() ? Version(DEFAULT_GL_VERSION) : global_settings.custom_gl_version;
@@ -254,7 +223,7 @@ void set_multidraw_setting() { // should be called after init_gles_target()
         global_settings.multidraw_mode = multidraw_mode_t::Auto;
         break;
     }
-    LOG_V("[MobileGlues] Setting: multidrawMode               = %s", draw_mode_str.c_str())
+    LOG_V("[MobileGL] Setting: multidrawMode is unsupported. Fallback to BaseVertex."/*, draw_mode_str.c_str()*/)
 }
 
 void init_settings_post() {
@@ -265,56 +234,39 @@ void init_settings_post() {
 
     switch (global_settings.multidraw_mode) {
     case multidraw_mode_t::PreferIndirect:
-        LOG_V("multidrawMode = PreferIndirect")
         if (indirect) {
             global_settings.multidraw_mode = multidraw_mode_t::PreferIndirect;
-            LOG_V("    -> Indirect (OK)")
         } else if (basevertex) {
             global_settings.multidraw_mode = multidraw_mode_t::PreferBaseVertex;
-            LOG_V("    -> BaseVertex (Preferred not supported, falling back)")
         } else {
             global_settings.multidraw_mode = multidraw_mode_t::DrawElements;
-            LOG_V("    -> DrawElements (Preferred not supported, falling back)")
         }
         break;
     case multidraw_mode_t::PreferBaseVertex:
-        LOG_V("multidrawMode = PreferBaseVertex")
         if (basevertex) {
             global_settings.multidraw_mode = multidraw_mode_t::PreferBaseVertex;
-            LOG_V("    -> BaseVertex (OK)")
         } else if (multidraw) {
             global_settings.multidraw_mode = multidraw_mode_t::PreferMultidrawIndirect;
-            LOG_V("    -> MultidrawIndirect (Preferred not supported, falling back)")
         } else if (indirect) {
             global_settings.multidraw_mode = multidraw_mode_t::PreferIndirect;
-            LOG_V("    -> Indirect (Preferred not supported, falling back)")
         } else {
             global_settings.multidraw_mode = multidraw_mode_t::DrawElements;
-            LOG_V("    -> DrawElements (Preferred not supported, falling back)")
         }
         break;
     case multidraw_mode_t::DrawElements:
-        LOG_V("multidrawMode = DrawElements")
         global_settings.multidraw_mode = multidraw_mode_t::DrawElements;
-        LOG_V("    -> DrawElements (OK)")
         break;
     case multidraw_mode_t::Compute:
-        LOG_V("multidrawMode = Compute")
         global_settings.multidraw_mode = multidraw_mode_t::Compute;
-        LOG_V("    -> Compute (OK)")
         break;
     case multidraw_mode_t::Auto:
     default:
-        LOG_V("multidrawMode = Auto")
         if (basevertex) {
             global_settings.multidraw_mode = multidraw_mode_t::PreferBaseVertex;
-            LOG_V("    -> BaseVertex (Auto detected)")
         } else if (multidraw) {
             global_settings.multidraw_mode = multidraw_mode_t::PreferMultidrawIndirect;
-            LOG_V("    -> MultidrawIndirect (Auto detected)")
         } else if (indirect) {
             global_settings.multidraw_mode = multidraw_mode_t::PreferIndirect;
-            LOG_V("    -> Indirect (Auto detected)")
         } else {
             global_settings.multidraw_mode = multidraw_mode_t::DrawElements;
             LOG_V("    -> DrawElements (Auto detected)")
@@ -339,76 +291,6 @@ std::string dump_settings_string(std::string prefix) {
         ss << "Full";
         break;
     }
-    ss << "\n";
-
-    ss << prefix << "ExtGL43: " << (global_settings.ext_gl43 ? "True" : "False") << "\n";
-    ss << prefix << "ExtComputeShader: " << (global_settings.ext_compute_shader ? "True" : "False") << "\n";
-    ss << prefix << "ExtTimerQuery: " << (global_settings.ext_timer_query ? "True" : "False") << "\n";
-    ss << prefix << "ExtDirectStateAccess: " << (global_settings.ext_direct_state_access ? "True" : "False") << "\n";
-    ss << prefix << "MaxGlslCacheSize: " << (global_settings.max_glsl_cache_size / 1024 / 1024) << "MB\n";
-
-    ss << prefix << "MultidrawMode: ";
-    switch (global_settings.multidraw_mode) {
-    case multidraw_mode_t::Auto:
-        ss << "Auto";
-        break;
-    case multidraw_mode_t::PreferIndirect:
-        ss << "Indirect (glDrawElementsIndirect)";
-        break;
-    case multidraw_mode_t::PreferBaseVertex:
-        ss << "BaseVertex (glDrawElementsBaseVertex)";
-        break;
-    case multidraw_mode_t::PreferMultidrawIndirect:
-        ss << "MultidrawIndirect (glMultiDrawElementsIndirect)";
-        break;
-    case multidraw_mode_t::DrawElements:
-        ss << "DrawElements (glDrawElements with per-draw CPU rebase)";
-        break;
-    case multidraw_mode_t::Compute:
-        ss << "Compute (glDrawElements with compute-shader rebase)";
-        break;
-    default:
-        ss << "Unknown";
-        break;
-    }
-    ss << "\n";
-
-    ss << prefix << "AngleDepthClearFixMode: "
-       << (global_settings.angle_depth_clear_fix_mode == AngleDepthClearFixMode::Disabled ? "Disabled" : "Enabled")
-       << "\n";
-
-    ss << prefix << "BufferCoherentAsFlush: " << (global_settings.buffer_coherent_as_flush ? "True" : "False") << "\n";
-
-    ss << prefix << "CustomGLVersion: "
-       << ((GLVersion.toInt(2) == DEFAULT_GL_VERSION) ? "(Default)" : std::to_string(GLVersion.toInt(2))) << "\n";
-
-    ss << prefix << "Fsr1Setting: ";
-
-    switch (global_settings.fsr1_setting) {
-    case FSR1_Quality_Preset::Disabled:
-        ss << "Disabled";
-        break;
-    case FSR1_Quality_Preset::UltraQuality:
-        ss << "UltraQuality";
-        break;
-    case FSR1_Quality_Preset::Quality:
-        ss << "Quality";
-        break;
-    case FSR1_Quality_Preset::Balanced:
-        ss << "Balanced";
-        break;
-    case FSR1_Quality_Preset::Performance:
-        ss << "Performance";
-        break;
-    default:
-        ss << "Unknown";
-        break;
-    }
-    ss << "\n";
-
-    ss << prefix << "HideMGEnvLevel: " << ((global_settings.hide_mg_env_level == HideMGEnvLevel::Disabled)
-        ? "Disabled" : std::to_string(static_cast<int>(global_settings.hide_mg_env_level)));
-
     ss << "\n";
 
     return ss.str();
