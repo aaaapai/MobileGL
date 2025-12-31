e7d9da42dc2b8e5ed93c742c9bf969d8441da564
BZLZHH <admin@bzlzhh.top>
2025-12-31 21:49:54 +0800

    [Chore] (All): Adjust copyright declaration.

--- MobileGL/MG_State/GLState/ErrorState/ErrorCode.h
+++ MobileGL/MG_State/GLState/ErrorState/ErrorCode.h
@@ -1,5 +1,5 @@
 // MobileGL - MobileGL/MG_State/GLState/ErrorState/ErrorCode.h
-// Copyright (C) 2025 MobileGL-Dev
+// Copyright (c) 2025 MobileGL-Dev
 // Licensed under the GNU Lesser General Public License v2.1:
 // http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
 // SPDX-License-Identifier: LGPL-2.1-only
