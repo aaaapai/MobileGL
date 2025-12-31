8ed8efe347edd91265d2f66eb0f6797c6ec4c2a5
BZLZHH <admin@bzlzhh.top>
2025-12-31 15:45:34 +0800

    [Chore] (All): Add standardized source file header and update script.

--- MobileGL/MG_Impl/EGLImpl/EGLWrapper/EGLWrapper.cpp
+++ MobileGL/MG_Impl/EGLImpl/EGLWrapper/EGLWrapper.cpp
@@ -1,3 +1,10 @@
+// MobileGL - MobileGL/MG_Impl/EGLImpl/EGLWrapper/EGLWrapper.cpp
+// Copyright (C) 2025 MobileGL-Dev
+// Licensed under the GNU Lesser General Public License v2.1:
+// http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+// SPDX-License-Identifier: LGPL-2.1-only
+// End of Source File Header
+
 #include "EGLWrapper.h"
 #include <Config.h>
 #include <MG_Util/BackendLoaders/OpenGL/Loader.h>
--- MobileGL/MG_Util/Converters/GLToMG/TextureEnumConverter.cpp
+++ MobileGL/MG_Util/Converters/GLToMG/TextureEnumConverter.cpp
@@ -1,3 +1,10 @@
+// MobileGL - MobileGL/MG_Util/Converters/GLToMG/TextureEnumConverter.cpp
+// Copyright (C) 2025 MobileGL-Dev
+// Licensed under the GNU Lesser General Public License v2.1:
+// http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+// SPDX-License-Identifier: LGPL-2.1-only
+// End of Source File Header
+
 #include "TextureEnumConverter.h"
 #include "GL/glext.h"
 #include "MG_Util/Converters/GLToStr/GLEnumConverter.h"
