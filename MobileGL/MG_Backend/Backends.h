68a77345a8b921a85d9bf0105cde3c34cdfcfb7d
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-01-02 11:43:02 +0800

    Update Backends.h

--- MobileGL/MG_Backend/Backends.h
+++ MobileGL/MG_Backend/Backends.h
@@ -75,7 +75,7 @@ namespace MobileGL {
                 .RendererGLInfo =
                     {
                         // OpenGL Info
-                        .TargetGLVersion = {4, 3, 0},                      //   Target OpenGL Version
+                        .TargetGLVersion = {3, 3, 0},                      //   Target OpenGL Version
                         .TargetGLSLVersion = {4, 6, 0},                    //   Target Shading Language Version
                         .Extensions = {V_OpenGL30, V_OpenGL31, V_OpenGL32, //   OpenGL Extensions
                                        V_OpenGL33/*, V_OpenGL40, V_OpenGL41, V_OpenGL42, V_OpenGL43,*/ E_GL_ARB_draw_buffers_blend},
