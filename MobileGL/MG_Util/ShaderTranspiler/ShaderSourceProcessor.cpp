08a54d6ccceb6594bb21340f38c5dcdc4db6500a
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-01-18 13:31:24 +0800

    Update ShaderSourceProcessor.cpp

--- MobileGL/MG_Util/ShaderTranspiler/ShaderSourceProcessor.cpp
+++ MobileGL/MG_Util/ShaderTranspiler/ShaderSourceProcessor.cpp
@@ -35,7 +35,7 @@ namespace MobileGL {
                 const char* str_np = "noperspective";
                 const SizeT len_np = strlen(str_np);
                 SizeT noperspectivePos = source.find(str_np);
-                while (noperspectivePos != String::npos) {
+                while (noperspectivePos != String::npos && !std::getenv("LIBGL_ANGLE")) {
                     // + length of "\n"
                     source = source.replace(noperspectivePos, len_np, "");
                     noperspectivePos = source.find(str_np);
