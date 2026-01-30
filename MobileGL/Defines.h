37aa106f7b40376ca746c6929fe01dc3546f48b7
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-01-30 12:58:34 +0800

    Update Defines.h

--- MobileGL/Defines.h
+++ MobileGL/Defines.h
@@ -48,9 +48,7 @@
 
 // Require C++23
 // Clang/Android NDK still doesn't have support for that :(
-#if __cplusplus >= 202302L && !__ANDROID__
 #define MOBILEGL_LOG_ENABLE_STACKTRACE 0
-#endif
 
 #ifdef __ANDROID__
 #define MOBILEGL_LOG_FILE_PATH "/sdcard/MGL/latest.log"
