770afed5ce5ecdaef280567cfce7fead0c74e2f1
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-02-01 00:14:09 +0800

    Update multidraw.h

--- MobileGL/MG_Backend/DirectGLES/multidraw.h
+++ MobileGL/MG_Backend/DirectGLES/multidraw.h
@@ -10,3 +10,11 @@ void MultiDrawElements_indirect(GLenum mode, const GLsizei* count, GLenum type, 
 void MultiDrawElements_compute(GLenum mode, const GLsizei* count, GLenum type, const GLvoid* const* indices, GLsizei drawcount);
 
 }
+
+MOBILEGL_GL_API void glMultiDrawElements_indirect(GLenum mode, const GLsizei* count, GLenum type, const GLvoid* const* indices, GLsizei drawcount);
+
+MOBILEGL_GL_API void glMultiDrawElements_compute(GLenum mode, const GLsizei* count, GLenum type, const GLvoid* const* indices, GLsizei drawcount);
+
+MOBILEGL_GL_API void glMultiDrawElementsBaseVertex_indirect(GLenum mode, const GLsizei* count, GLenum type, const GLvoid* const* indices, GLsizei drawcount, const GLint* basevertex);
+
+MOBILEGL_GL_API void glMultiDrawElementsBaseVertex_compute(GLenum mode, const GLsizei* count, GLenum type, const GLvoid* const* indices, GLsizei drawcount, const GLint* basevertex);
