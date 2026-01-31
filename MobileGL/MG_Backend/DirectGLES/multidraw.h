a404462040a3918861f8825850fc795f36b7c660
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-02-01 00:01:18 +0800

    Update multidraw.h

--- MobileGL/MG_Backend/DirectGLES/multidraw.h
+++ MobileGL/MG_Backend/DirectGLES/multidraw.h
@@ -1,3 +1,6 @@
+#pragma once
+#include <Includes.h>
+
 namespace MobileGL::MG_Backend::DirectGLES {
 
 void MultiDrawElementsBaseVertex_indirect(GLenum mode, const GLsizei* count, GLenum type, const GLvoid* const* indices, GLsizei drawcount, const GLint* basevertex);
