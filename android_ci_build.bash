649bf92ae944ff6982feedcb1f07d1517dbd1371
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-02-03 17:10:18 +0800

    Update android_ci_build.bash

--- android_ci_build.bash
+++ android_ci_build.bash
@@ -6,8 +6,8 @@ cmake_build () {
   ANDROID_ABI=$1
   mkdir -p build
   cd build
-  cmake $GITHUB_WORKSPACE -DANDROID_PLATFORM=24 -DANDROID_ABI=$ANDROID_ABI -DCMAKE_ANDROID_STL_TYPE=c++_static -DCMAKE_SYSTEM_NAME=Android -DANDROID_TOOLCHAIN=clang -DCMAKE_MAKE_PROGRAM=$ANDROID_NDK_HOME/prebuilt/linux-x86_64/bin/make -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake
-  cmake --build . --config Release --parallel 6
+  $ANDROID_NDK_HOME/prebuilt/linux-x86_64/bin/make $GITHUB_WORKSPACE -DANDROID_PLATFORM=24 -DANDROID_ABI=$ANDROID_ABI -DCMAKE_ANDROID_STL_TYPE=c++_static -DCMAKE_SYSTEM_NAME=Android -DANDROID_TOOLCHAIN=clang -DCMAKE_MAKE_PROGRAM=$ANDROID_NDK_HOME/prebuilt/linux-x86_64/bin/make -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake
+  $ANDROID_NDK_HOME/prebuilt/linux-x86_64/bin/make --build . --config Release --parallel 6
   # 在bash中启用globstar
   shopt -s globstar
   $ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-strip $GITHUB_WORKSPACE/**/libMobileGL.so
