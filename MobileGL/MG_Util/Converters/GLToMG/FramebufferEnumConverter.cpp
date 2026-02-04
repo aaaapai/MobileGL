fd5cc2e5c10449ee3c8bbeb2ddc6ecd1c989655b
Swung0x48 <swung0x48@outlook.com>
2026-02-04 15:34:41 +0800

    [Feat] (MG_Util/Converters): add symbolic constants for default FBO into `ConvertGLEnumToFramebufferAttachmentType`

--- MobileGL/MG_Util/Converters/GLToMG/FramebufferEnumConverter.cpp
+++ MobileGL/MG_Util/Converters/GLToMG/FramebufferEnumConverter.cpp
@@ -36,6 +36,14 @@ namespace MobileGL {
                     return FramebufferAttachmentType::Depth;
                 case GL_STENCIL_ATTACHMENT:
                     return FramebufferAttachmentType::Stencil;
+                case GL_FRONT_LEFT:
+                    return FramebufferAttachmentType::FrontLeft;
+                case GL_FRONT_RIGHT:
+                    return FramebufferAttachmentType::FrontRight;
+                case GL_BACK_LEFT:
+                    return FramebufferAttachmentType::BackLeft;
+                case GL_BACK_RIGHT:
+                    return FramebufferAttachmentType::BackRight;
                 default:
                     return FramebufferAttachmentType::Unknown;
             }
