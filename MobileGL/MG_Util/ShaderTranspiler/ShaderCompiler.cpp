93f08e251d1794f2baf2f46f6237fed04ce51aa1
Swung0x48 <swung0x48@outlook.com>
2026-02-05 15:59:14 +0800

    [Fix] (MG_Util/ShaderTranspiler): forcefully disable validator of spvtools

--- MobileGL/MG_Util/ShaderTranspiler/ShaderCompiler.cpp
+++ MobileGL/MG_Util/ShaderTranspiler/ShaderCompiler.cpp
@@ -230,11 +230,14 @@ namespace MobileGL {
             bool ShaderCompiler::SanitizeAndOptimizeBinary(const Vector<Uint32>& inputBinary,
                                                            Vector<uint32_t>& outputBinary) {
                 using namespace spvtools;
+                OptimizerOptions options;
+                options.set_run_validator(false);
+
                 Optimizer optimizer(SPV_ENV_UNIVERSAL_1_5);
 
                 optimizer.RegisterPass(EliminateFloatEqualsZeroPass::CreateEliminateFloatEqualsZeroPass());
 
-                return optimizer.Run(inputBinary.data(), inputBinary.size(), &outputBinary);
+                return optimizer.Run(inputBinary.data(), inputBinary.size(), &outputBinary, options);
             }
 
             Result<String> ShaderCompiler::DecompileShader(SpvcSession& session) {
